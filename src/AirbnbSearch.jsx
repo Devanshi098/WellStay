9e26c05a250da84f54ef134ab7b11b3c4593fe1a
Devanshi Dadwal <devanshidadwal2@gmail.com>
2024-05-02 11:50:15 +0530

    dist folder uploaded

--- src/AirbnbSearch.jsx
+++ src/AirbnbSearch.jsx
@@ -62,27 +62,30 @@ function AirbnbSearch() {
   };
 
   useEffect(() => {
-    // Fetch data on component mount or when dependencies change
+  
     handleSubmit();
-  }, []); // Empty dependency array means it only runs once on mount
+  }, []); 
 
   return (
     <div className="airbnb-search">
       <form className="search-form mt-6 flex-row " onSubmit={handleSubmit}>
-  {/* Your form input fields */}
-  {/* Example: */}
+
+  
   {/* Location */}
   <input className="w-40 bg-[#ff5a60] border border-black border-3 rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center rounded"placeholder="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />    
   {/* Check-in Date */}
   <input className="mt-2 w-40 bg-[#ff5a60] border border-black rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center" type="date" value={checkInDate} onChange={(e) => setCheckInDate(e.target.value)} />
   {/* Check-out Date */}
   <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3  rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center" type="date" value={checkOutDate} onChange={(e) => setCheckOutDate(e.target.value)} />
+  {/*no. of adults*/}
   <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3 rounded-md px-3 py-2 mr-10 text-gray-700 placeholder-black text-center" placeholder="Adults" type="text" value={adults} onChange={(e) => setadults(e.target.value)} />
+  {/* no. of children*/}
   <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3  rounded-md px-3 py-2 mr-10 text-gray-700 placeholder-black text-center" placeholder="Children" type="text" value={children} onChange={(e) => setchildren(e.target.value)} />
+
   <button className="mt-2 bg-[#ff5a60] w-40 border border-black border-3  text-black  py-2 px-4 rounded" type="submit text-center">Search</button>
 </form>
 {showImage && (
-        <img src="https://luchanik.files.wordpress.com/2014/09/20140725_110205.jpg?w=768" alt="Description of your image"  style={{ 
+        <img src="https://luchanik.files.wordpress.com/2014/09/20140725_110205.jpg?w=768" alt="Description of image"  style={{ 
           width: '100vw', 
           height: '100vh', 
           objectFit: 'cover',
@@ -115,7 +118,7 @@ function AirbnbSearch() {
               />
               <CardMedia 
   component="img"
-  style={{ height: '200px', width: '100%' ,margin:'auto' }} // Adjust height and width as needed
+  style={{ height: '200px', width: '100%' ,margin:'auto' }} 
   image={listing.images[0]}
   alt={listing.name}
 />
