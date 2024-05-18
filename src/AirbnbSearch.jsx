2e5e8f256f5f77307f0416709fce53c0c752e00a
Devanshi Dadwal <devanshidadwal2@gmail.com>
2024-05-19 01:30:27 +0530

    interface visuals updates

--- src/AirbnbSearch.jsx
+++ src/AirbnbSearch.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 function AirbnbSearch() {
   const [location, setLocation] = useState('');
@@ -24,15 +15,7 @@ function AirbnbSearch() {
   const [adults, setadults] = useState('');
   const [children, setchildren] = useState('');
   const [showImage, setShowImage] = useState(true);
-  
-  // const handleExpandClick = (id) => {
-  //   setListings(prevListings => prevListings.map(listing => {
-  //     if (listing.id === id) {
-  //       return { ...listing, expanded: !listing.expanded };
-  //     }
-  //     return listing;
-  //   }));
-  // };
+  const [selectedListing, setSelectedListing] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -42,7 +25,7 @@ function AirbnbSearch() {
         method: 'GET',
         headers: {
           'X-RapidAPI-Key': '918f620157msh114346aeb04e72fp1e766bjsnb71ae37e7c98',
-    'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
+          'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
         }
       };
 
@@ -51,111 +34,121 @@ function AirbnbSearch() {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      const formattedListings = data.results.map(listing => ({ ...listing, expanded: false }));
-      setListings(formattedListings);
+      setListings(data.results);
       setShowImage(false);
-
-    } 
-    catch (error) {
+    } catch (error) {
       console.error('Error fetching Airbnb listings:', error);
     }
   };
 
   useEffect(() => {
-  
     handleSubmit();
-  }, []); 
+  }, []);
+
+  const handleCardClick = (listing) => {
+    setSelectedListing(listing);
+  };
+
+  const handleClose = () => {
+    setSelectedListing(null);
+  };
 
   return (
     <div className="airbnb-search">
-      <form className="search-form mt-6 flex-row " onSubmit={handleSubmit}>
-
-  
-  {/* Location */}
-  <input className="w-40 bg-[#ff5a60] border border-black border-3 rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center rounded"placeholder="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />    
-  {/* Check-in Date */}
-  <input className="mt-2 w-40 bg-[#ff5a60] border border-black rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center" type="date" value={checkInDate} onChange={(e) => setCheckInDate(e.target.value)} />
-  {/* Check-out Date */}
-  <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3  rounded-md px-3 py-2 mr-10 text-black placeholder-black text-center" type="date" value={checkOutDate} onChange={(e) => setCheckOutDate(e.target.value)} />
-  {/*no. of adults*/}
-  <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3 rounded-md px-3 py-2 mr-10 text-gray-700 placeholder-black text-center" placeholder="Adults" type="text" value={adults} onChange={(e) => setadults(e.target.value)} />
-  {/* no. of children*/}
-  <input className="mt-2 bg-[#ff5a60] w-40 border  border-black border-3  rounded-md px-3 py-2 mr-10 text-gray-700 placeholder-black text-center" placeholder="Children" type="text" value={children} onChange={(e) => setchildren(e.target.value)} />
-
-  <button className="mt-2 bg-[#ff5a60] w-40 border border-black border-3  text-black  py-2 px-4 rounded" type="submit text-center">Search</button>
-</form>
-{showImage && (
-        <img src="https://luchanik.files.wordpress.com/2014/09/20140725_110205.jpg?w=768" alt="Description of image"  style={{ 
-          width: '100vw', 
-          height: '100vh', 
+      <form className="search-form mt-6 flex-row" onSubmit={handleSubmit}>
+        {/* Location */}
+        <input className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px] mr-4" placeholder="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+        {/* Check-in Date */}
+        <input className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px] mr-4" type="date" value={checkInDate} onChange={(e) => setCheckInDate(e.target.value)} />
+        {/* Check-out Date */}
+        <input className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px] mr-4" type="date" value={checkOutDate} onChange={(e) => setCheckOutDate(e.target.value)} />
+        {/* No. of adults */}
+        <input className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-4 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px] mr-4" placeholder="Adults" type="text" value={adults} onChange={(e) => setadults(e.target.value)} />
+        {/* No. of children */}
+        <input className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px] mr-4" placeholder="Children" type="text" value={children} onChange={(e) => setchildren(e.target.value)} />
+        <button className="items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px]">Search</button>
+      </form>
+      {showImage && (
+        <img src="https://images.pexels.com/photos/2468773/pexels-photo-2468773.jpeg" alt="Description of image" style={{
+          width: '100vw',
+          height: '100vh',
           objectFit: 'cover',
           position: 'fixed',
           top: 0,
           left: 0,
           zIndex: -1
-        }}  />
-
+        }} />
       )}
-      <br/>
-      <br/>
+      <br />
+      <br />
       <Grid container spacing={2}>
         {listings.map(listing => (
-          <Grid item xs={6} sm={4} md={3} key={listing.id}>
-            <Card style={{ height: '100%' }}> {/* Set card height to 100% */}
-              <CardHeader 
-                avatar={
-                  <Avatar sx={{ bgcolor: red[500] }} aria-label="profile">
-                    🤚
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title={listing.name}
-                subheader={listing.city}
+          <Grid item xs={12} sm={7} md={3} key={listing.id}>
+            <Card onClick={() => handleCardClick(listing)} style={{ cursor: 'pointer', height: '100%' }}>
+              <CardMedia
+                component="img"
+                style={{ height: '200px',borderRadius:'10px' }}
+                image={listing.images[0]}
+                alt={listing.name}
               />
-              <CardMedia 
-  component="img"
-  style={{ height: '200px', width: '100%' ,margin:'auto' }} 
-  image={listing.images[0]}
-  alt={listing.name}
-/>
-
-              <CardContent style={{ height: '100px' }}> {/* Set fixed height for card content */}
+              <CardContent>
+                <Typography variant="h6" color="text.primary" gutterBottom>
+                  {listing.name}
+                </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  {listing.description}
+                  {listing.city}
                 </Typography>
               </CardContent>
-              <Typography variant="body2" color="text.secondary"> RATING: {listing.rating} OUT OF {listing.reviewsCount}</Typography>
-              <Typography variant="body2" color="text.secondary"> INR {listing.price["rate"]} per night.</Typography>
-              <Typography variant="body2" color="text.secondary"> BEDROOMS: {listing.bedrooms} </Typography>
-              <Typography variant="body2" color="text.secondary"> LOCATION: {listing.address} </Typography>
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-                {/* <IconButton
-                  onClick={() => handleExpandClick(listing.id)}
-                  aria-expanded={listing.expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </IconButton> */}
-              </CardActions>
-              <Collapse in={listing.expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  {/* Additional information, if needed */}
-                </CardContent>
-              </Collapse>
             </Card>
           </Grid>
         ))}
       </Grid>
+      {selectedListing && (
+        <Modal
+          open={Boolean(selectedListing)}
+          onClose={handleClose}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+        >
+          <Box sx={{ 
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            transform: 'translate(-50%, -50%)', 
+            width: 400, 
+            bgcolor: 'background.paper', 
+            border: '2px solid #000', 
+            boxShadow: 24,
+            borderRadius:'30px', 
+            p: 4 
+          }}>
+            <Typography id="modal-title" variant="h6" component="h2">
+              {selectedListing.name}
+            </Typography>
+            <CardMedia
+              component="img"
+              style={{ height: '200px', width: '100%', margin: 'auto',borderRadius:'30px' }}
+              image={selectedListing.images[0]}
+              alt={selectedListing.name}
+            />
+            <Typography id="modal-description" sx={{ mt: 2 }}>
+              {selectedListing.description}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              RATING: {selectedListing.rating} OUT OF {selectedListing.reviewsCount}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              INR {selectedListing.price["rate"]} per night.
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              BEDROOMS: {selectedListing.bedrooms}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              LOCATION: {selectedListing.address}
+            </Typography>
+          </Box>
+        </Modal>
+      )}
     </div>
   );
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,7 +3,15 @@ import { GiFishingBoat, GiMineExplosion } from "react-icons/gi";
 import { ImKey } from "react-icons/im";
 import { RiAliensFill } from "react-icons/ri";
 import { BsFillTreeFill } from "react-icons/bs";
-import Filter from "./Filter";
+
+const Filter = ({ icon, title }) => {
+  return (
+    <div className="flex items-center text-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-gray-900 duration-200 ease-out gap-2 py-2 px-4 sm:px-6 rounded-lg shadow-md text-[14px] sm:text-[16px]">
+      {icon}
+      {title}
+    </div>
+  );
+};
 
 const Filters = () => {
   const sorting = [
@@ -14,12 +22,10 @@ const Filters = () => {
     { title: "Forest", icon: <BsFillTreeFill /> },
   ];
   return (
-    <div className="   ">
-      <div className="flex justify-start  gap-3 sm:gap-4  mt-4  ">
-        {sorting.map((obj) => (
-          <Filter title={obj.title} icon={obj.icon} />
-        ))}
-      </div>
+    <div className="flex justify-start gap-3 sm:gap-4 mt-4">
+      {sorting.map((obj, index) => (
+        <Filter key={index} title={obj.title} icon={obj.icon} />
+      ))}
     </div>
   );
 };
